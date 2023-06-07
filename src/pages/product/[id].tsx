fb7ac2f7c083cbd58f97b60e5d56f5770445cb3d
Marcus Davanço <md.daves@gmail.com>
2023-06-07 17:47:16 -0300

    feat: Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,27 @@
 import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
 
 export default function Product() {
   const { query } = useRouter();
-  return <h1>Product: {JSON.stringify(query)} </h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,99</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Reprehenderit eum magnam eaque sit excepturi natus omnis vel, dolorem
+          officia eos, dolor perspiciatis, assumenda similique aspernatur porro
+          asperiores facere earum doloremque?
+        </p>
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
