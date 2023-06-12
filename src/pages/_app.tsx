fe6bd059f8ad0b1e461371701d64f7946e1c2f92
Marcus Davanço <md.daves@gmail.com>
2023-06-12 13:44:21 -0300

    feat: use-shopping-cart provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,16 +4,24 @@ import { globalStyles } from "../styles/global";
 import logoImg from "../assets/logo.svg";
 import { Container, Header } from "../styles/pages/app";
 import Image from "next/image";
+import { CartProvider } from "use-shopping-cart";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Container>
-      <Header>
-        <Image src={logoImg.src} width={130} height={85} alt="logo" />
-      </Header>
-      <Component {...pageProps} />
-    </Container>
+    <CartProvider
+      cartMode="checkout-session"
+      currency="BRL"
+      shouldPersist={true}
+      stripe={process.env.STRIPE_PUBLIC_KEY}
+    >
+      <Container>
+        <Header>
+          <Image src={logoImg.src} width={130} height={85} alt="logo" />
+        </Header>
+        <Component {...pageProps} />
+      </Container>
+    </CartProvider>
   );
 }
